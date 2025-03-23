634ff56ae1c29ecadaf1aa7f933c27aaa78653b1
RobinHawiz <Robin.Hawiz.6670@student.uu.se>
2025-03-23 23:26:57 +0100

    ✨ feat: Created a main function that handles the logic for fetching and storing course data.
    Created `main.ts` to handle core application functionality, such as fetching course data and storing it in `localStorage`.

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,1 +1,28 @@
+// Styles
 import "@styles/style.scss";
+// Interfaces
+import { ICourseInfo } from "@ts/ICourseInfo";
+// Classes
+import { DataList } from "@ts/DataList";
+// Functions
+import { fetchData } from "@ts/fetch-data";
+import { getLocalStorageData, updateLocalStorage } from "@ts/local-storage";
+
+async function main(): Promise<void> {
+  let data: DataList<ICourseInfo>;
+  let localStorageData: DataList<ICourseInfo> = new DataList(
+    getLocalStorageData<ICourseInfo>("Courses")
+  );
+  // Use local storage data if available, otherwise fetch new data.
+  if (localStorageData.getDataList().length !== 0) data = localStorageData;
+  else {
+    data = new DataList<ICourseInfo>(
+      await fetchData<Array<ICourseInfo>>(
+        "https://webbutveckling.miun.se/files/ramschema_ht24.json"
+      )
+    );
+    updateLocalStorage<ICourseInfo>("Courses", data.getDataList());
+  }
+}
+
+main();
